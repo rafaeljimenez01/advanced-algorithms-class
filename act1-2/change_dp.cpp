5afe30d0c29c785d8ecdb4e6b7e0ab35caa595f1
Joshua Hdz <joshua.hdz.reza@gmail.com>
2021-08-27 12:01:13 -0400

    add comments

--- act1-2/change_dp.cpp
+++ act1-2/change_dp.cpp
@@ -1,44 +1,103 @@
 #include <iostream>
 #include <vector>
-#include <utility>
-#include <bits/stdc++.h>
 
-void get_change(std::vector<int> &coins, std::vector<std::pair<int, int*>> &minCoins, const int &change) {
-  for(int money = 1; money <= change; ++money) {
-    for(int &coin : coins) {
-      if(coin <= money && minCoins[money - coin].first < minCoins[money].first) {
-          minCoins[money].first = minCoins[money - coin].first + 1;
-          minCoins[money].second = &coin;
-      }
-    }
+/*******************************************************************************
+ * Prints solution.
+ * 
+ * Time complexity O(w).
+ * Space complexity O(w).
+ * 
+ * Where w is the size of the solution or the number of needed coins.
+ ******************************************************************************/
+void print(const std::vector<int*> &minCoins, int change) {
+  while(change > 0) {
+    std::cout << *minCoins[change] << std::endl;
+    change -= *minCoins[change];
   }
 }
 
-int main() {
-  int n,
-      p,
-      q,
-      change;
 
+/*******************************************************************************
+ *   Finds the right mix of coins that add up to the needed change with the
+ * least amount of coins.
+ * 
+ *   Utilizes a dynamic programing aproach. More specifically a bottom up
+ * strategy. In other words, to find the solution for change = 10 the algorithm
+ * requiers to find the solution for change 1, 2, 3, 4... 9 in this order and 
+ * saves it in memory for later use. For each sub-problem it tries every coin.
+ * Thus:
+ *  - Time Complexity O(n * m)
+ *  - Space Complexity O(n)
+ * 
+ *    Where n is the amount of change and m is the amount of different coin's
+ * denomination.
+*******************************************************************************/
+std::vector<int*> changeDp(std::vector<int> &coins, const int &change) {
+  std::vector<int*> solution(change + 1, nullptr);
+
+  for(size_t money = 1; money <= change; ++money) {
+    for(int &coin : coins) {
+      if(coin <= money && solution[money - coin] <solution[money]) {
+          solution[money] = &coin;
+      }
+    }
+  }
+
+  return solution;
+}
+
+std::vector<int*> changeGreedy(std::vector<int> coins, int m) {
+
+  // sort(coins.begin(), coins.end(), std::greater<int>());
+  int current = 0;
+  std::vector<int*> solution;
+
+  while (m > 0){
+    if (m - coins[current] >= 0){
+      solution.push_back(&coins[current]);
+      m -= coins[current];
+    }
+    else{
+      current += 1;
+      if (current >= coins.size()) {
+        std::cout << "u trying to trick me nigga" << std::endl;
+        break;
+      }
+    }
+  }
+
+  return solution;
+
+}
+
+
+int main() {
+int n,        //number of coins
+    p,        //price of product
+    q,        //money received
+    change;   //change to give 
+
+  //creates a vector with every different coin denomination
+  std::cout << "Enter number of diferent denominations" << std::endl;
   std::cin >> n;
+  std::cout << "Enter denominations now" << std::endl;
   std::vector<int> coins(n);
   for(int &coin : coins) {
     std::cin >> coin;
   }
 
+  //Calculates the change and determains the minimum number of coins to complete the change
+  std::cout << "Enter price" << std::endl;
   std::cin >> p;
+  std::cout << "Enter amount received" << std::endl;
   std::cin >> q;
   change = p - q;
-  std::vector<std::pair<int, int*>> minCoins(change + 1, std::make_pair(INT_MAX, nullptr));
-  minCoins[0].first = 0;
 
-  get_change(coins, minCoins, change);
+  std::cout << "Dynamic programing solution:" << std::endl;
+  print(changeDp(coins, change), change);
 
-
-  while(change > 0) {
-    std::cout << *minCoins[change].second << std::endl;
-    change -= *minCoins[change].second;
-  }
+  std::cout << "Greedy solution:" << std::endl;
+  print(changeGreedy(coins, change), change);
 
   return 0;
 }
