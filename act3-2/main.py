a2b40b19380c61c102252161cddf8eca580224e8
Joshua Hdz <joshua.hdz.reza@gmail.com>
2021-09-30 19:42:49 -0400

    initial push for act3-2

---
+++ act3-2/main.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def relax(u, v, distance, current_cost):
+    pass
+
+def dijkstra(adjacency_matrix, origin):
+    pass
+
+if __name__ == "__main__":
+    adjacency_matrix = []
+
+    for _ in range(int(input())):
+        row = input().split(' ')
+        adjacency_matrix.append([int(edge) for edge in row])
+
+
+}
+
+    
+
+
+    
+
+    
+
+    
