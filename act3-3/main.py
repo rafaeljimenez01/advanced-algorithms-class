8c95a88954daa56b0c92b74d563436da974b16cf
rafaeljimenez01 <rafaeljimenez_01@hotmail.com>
2021-10-07 16:48:12 -0500

    Test cases completed and commets aswell

--- act3-3/main.py
+++ act3-3/main.py
@@ -12,6 +12,21 @@ Rafael Jimenez A01637850
 Joshua Hernandez A01246538
 
 '''
+
+
+# INPUT:
+#   - elements: the possible elements to be added to the knapsack in format [value, weight]
+#   - knapsack_cap: int, max weight available
+# OUTPUT:
+#   - solution to the knapsack problem.
+# DESCRIPTION:
+#   Given weights and values of n items, put these items 
+#   in a knapsack of capacity W to get the maximum total value in the knapsack. (gfg https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/)
+# Time Complexity: 
+#   O(N*W) where N is the number of different elements and W is the knapsack_cap
+# Space Complexity:
+#  O(N*W) where N is the number of different elements and W is the knapsack_cap
+
 def optimal_profit(elements, knapsack_cap):
     # initiate cost table (item amount * knapsack's capasity + 1 matrix).
     cost_table = [[0 for _ in range(knapsack_cap + 1)] for _ in range(len(elements))]
@@ -30,9 +45,22 @@ def optimal_profit(elements, knapsack_cap):
                     )
             else:
                 cost_table[item][current_cap] = cost_table[item - 1][current_cap]
- 
+    
+    
     # Return the lower right most value in the cost table.
     return cost_table[len(elements) - 1][knapsack_cap]
+
+
+# INPUT:
+#   none
+# OUTPUT:
+#   none
+# DESCRIPTION:
+#   Reads the test cases from test.txt
+# Time Complexity: 
+#   O(N*W) because we call the function optimal_solution.
+# Space Complexity:
+#   O(1) 
 
 if __name__ == "__main__":
 
@@ -53,12 +81,12 @@ if __name__ == "__main__":
             for item in elements:
                 item[1] = int(reader.readline())
 
+            
             knapsack_cap = int(reader.readline())
 
             print(optimal_profit(elements, knapsack_cap))
 
-            # Next test set up.
-            
+            # Next test set up.            
             element_amount = reader.readline()
 
 '''
@@ -73,8 +101,12 @@ Test case 2:
 What happens if all the possible items its heavier that the max capacity
 
 Test case 3:
-
+Weight is a big number to test if there is going to be any memory or index issues and
+to take advantage of the really optimal bottom-up(dp) implementation.
 
 Test case 4:
+In this example the greedy approach would fail and we would like to prove that dp
+approach is going to take the best possible solution always.
 
+Greedy would have taken the first value and the solution its the second + third one
 '''
